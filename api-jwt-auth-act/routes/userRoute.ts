836b80662fcbe6705e3ecf8cc8e4c70e4eca9432
Kentward Maratas <144243351+23000003@users.noreply.github.com>
2024-11-15 21:26:36 +0800

    im edging this refloging

--- api-jwt-auth-act/routes/userRoute.ts
+++ api-jwt-auth-act/routes/userRoute.ts
@@ -8,10 +8,11 @@ const router : Router = Router();
 
 const JWT_SECRET : string = 'supersecretkey';
 
-const users = [
-    { id: 1, username: 'admin', password: 'admin_password', role: 'admin' },
-    { id: 2, username: 'user', password: '$2a$10$xyz1234', role: 'user' },
+const users = [   //admin_password     //user_password          (created my encrypted password mock as the given is returning false)
+    { id: 1, username: 'admin', password: '$2a$10$3XeiDzWLReC0f6Msfy.Ym.aJfzkN1GYdMN6A.uB.1xi/iajoQV3uK', role: 'admin' },
+    { id: 2, username: 'user', password: '$2a$10$YaTXSvO5cIVOO96DDOm/8ec2iPFY4hjBeJ1FrriNLDVi7efaR/3oi', role: 'user' },
 ];
+
 
 // Login route
 router.post('/login', (req: Request, res: Response) : Response => {
@@ -20,12 +21,22 @@ router.post('/login', (req: Request, res: Response) : Response => {
     // Find user by username
     const user = users.find(u => u.username === username);
     if (!user) {
-        console.log("HERE2#!#!#!")
         return res.status(401).send('Invalid username or password');
     }
 
-    // Validate password
-    const isMatch = bcrypt.compare(user.password, password); //compareSync always returns false so ichange it to compare
+    // const Comp = async(): Promise<void> =>{
+    //     const salt = await bcrypt.genSalt();
+    //     const hashed = await bcrypt.hash(password, salt);
+    //     const isMatch = bcrypt.compareSync(password, "$2a$10$3XeiDzWLReC0f6Msfy.Ym.aJfzkN1GYdMN6A.uB.1xi/iajoQV3uK");
+    //     console.log(isMatch)
+    //     console.log(password, hashed);
+    // }
+
+    // Comp();
+
+    //Validate password
+    const isMatch = bcrypt.compareSync(password, user.password);
+
     if (!isMatch) {
         console.log("HERE")
         return res.status(401).send('Invalid username or password');
@@ -34,7 +45,10 @@ router.post('/login', (req: Request, res: Response) : Response => {
     // Generate JWT token
     const token = jwt.sign({ userId: user.id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
 
-    return res.json({ token });
+    // Generate Refresh Token
+    const refreshToken = jwt.sign({ userId: user.id, isRefreshToken: "true" }, JWT_SECRET, { expiresIn: '1d'});
+
+    return res.json({ token, refreshToken });
 }); 
 
 declare global {
@@ -63,6 +77,30 @@ router.get('/admin', authenticateToken, authorizeRole('admin'), (req: Request, r
 // User-only route
 router.get('/user', authenticateToken, authorizeRole('user'), (req: Request, res: Response) : void => {
     res.json({ message: 'Welcome User! This route is restricted to users only.' });
+});
+
+
+// Refresh-token route
+router.get('/refresh', authenticateToken, (req: Request, res: Response): Response | undefined =>{
+    
+    const refreshToken = <string>req.headers['refreshtoken'];
+    const user = req.user;
+
+    if(!refreshToken){
+        return res.send("No Refresh Token").status(403);
+    }
+
+    // Take away "Bearer " string
+    const newToken : string = refreshToken.split(" ")[1];
+
+    jwt.verify(newToken, JWT_SECRET, (err: any) : Response => {
+        if (err) return res.status(403).send('Invalid refresh token');
+        
+        const newToken = jwt.sign({ userId: user.userId, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+        
+        return res.status(200).json({ newToken })
+    });
+    
 });
 
 
---
+++ supa-edge/functions/send-email/index.ts
@@ -0,0 +1,52 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+const RESEND_API_KEY = Deno.env.get("_RESEND_API_KEY") as string;
+
+const handler = async (_request: Request): Promise<Response> => {
+
+  const { email, course } = await _request.json();
+
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: Deno.env.get("_RESEND_DOMAIN") as string,
+      to: email.toString(),
+      subject: 'Enrollment Successful',
+      html: `<strong>You are now Enrolled in course ${course.toString()} !</strong>`,
+    }),
+  })
+
+  const data = await res.json()
+
+  return new Response(JSON.stringify({data}), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+}
+
+Deno.serve(handler)
+
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/send-email' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
